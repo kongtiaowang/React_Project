b587765ef0c59dfaf7bedc19cfa7007985222c5c
Wang Shen <kongtiaowangshen@gmail.com>
2019-04-30 15:33:12 -0400

    todo 8-14

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = (props) => {
+	let ingredientslist = Object.keys(props.ingredients)
+		.map( (igKey) => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return (<BurgerIngredient type={igKey} key={igKey + i} />);
+			});
+		}).reduce((arr,el) => { return arr.concat(el)},[]
+		);
+	console.log(ingredientslist);
+	if (ingredientslist.length === 0) {
+		ingredientslist = <p>Please start adding ingredients!</p>;
+		console.log(ingredientslist);
+	}
+ 	return (
+		<div className={classes.Burger}>
+   			<BurgerIngredient type="bread-top" />
+            {ingredientslist}
+   			<BurgerIngredient type="bread-bottom" />
+		</div>
+	);
+}
+
+export default Burger;
